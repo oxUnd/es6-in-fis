f08b02daed40edfc2f83a3647e2766f24a3b7544
xiangshouding <fansekey@gmail.com>
2015-06-19 15:58:54 +0800

    module extend other module

---
+++ es6/module/_a.js
@@ -0,0 +1,7 @@
+export function a_1 () {
+    console.log('a_1');
+};
+
+export var a_2 = `
+    multi string
+`;
--- es6/module/asyncM.js
+++ es6/module/asyncM.js
@@ -1,7 +1,12 @@
+
+import {a_1, a_2} from "./_a";
+
 export function a () {
     console.log('a');
+    a_1();
 }
 
 export function b () {
     console.log('b');
+    console.log(a_2);
 }
